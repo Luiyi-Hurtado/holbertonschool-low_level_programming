0e652acd2e9e2b1e512d666857594f89df412cf1
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-13 16:40:47 -0500

    created 5-printf.c v1

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Descrition: Use the printf function for display
+*with proper grammar, but the outcome is a piece of art,
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return  (0);
+}
