5d7ff960caf3b9d8a27dc77bdb0aa0c9d124250b
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-13 16:26:33 -0500

    created 4-puts.c v4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzlei";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
 
