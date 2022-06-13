479de27b3d9a686afd93317b6970b1108e187dec
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-13 17:38:18 -0500

    created 6-size.c v2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 {
 	int a;
 
-	printf("Size of a char: %d\n" sizeof(a) "byte(s)");
-	printf("Size of an int: %d\n" sizeof(int) "byte(s)");
-	printf("Size if a long int: %d\n" sizeof(long int) "byte(s)");
-	printf("Size of a long long int: %d\n" sizeof(long long int) "byte(s)");
-	printf("Size of a float: %d\n" sizeof(float) "byte(s)");
+	printf("Size of a char: " sizeof(a) "byte(s)");
+	printf("Size of an int: " sizeof(int) "byte(s)");
+	printf("Size if a long int:" sizeof(long int) "byte(s)");
+	printf("Size of a long long int: " sizeof(long long int) "byte(s)");
+	printf("Size of a float: " sizeof(float) "byte(s)");
 
 	return (0);
 }
