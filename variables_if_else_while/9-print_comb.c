33c6255a3468ad0d57fcad93e05f064a99fb742a
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-14 16:39:22 -0500

    created 9-print_comb.c v1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: print a single numbers
+*the numbers separated by ', ', can
+*only used putchcar and can only use
+*four times in the code.
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(' ');
+		putchar(i);
+		putchar(',');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
