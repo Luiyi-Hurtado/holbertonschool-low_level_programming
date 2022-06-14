087a30eeb730b88b09c82d60d8d7c36b11d868b9
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-13 21:22:41 -0500

    created 101-quote.c v4

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -12,9 +12,7 @@
 
 int main(void)
 {
-	char Mystr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	putchar(Mystr);
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19")
 
 	return (1);
 }
