69fe27a8d7eaa0e06acf7c4799f666644cc039b0
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-13 20:38:13 -0500

    created 101-quote.c v1

---
+++ hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: Use putchar function for display
+*and that piece of art is useful" - Dora Korpar, 2015-10-19
+*
+*Return: 1
+*
+*/
+
+int main(void)
+{
+	char Mystr[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+
+	putchar(Mystr);
+}
+
