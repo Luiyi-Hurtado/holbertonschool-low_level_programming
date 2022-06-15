b622d83f6e64144fc5f2c0ae5b1be5adad61c91e
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-15 17:44:28 -0500

    created 2-print_alphabet_x10.c v1

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - Entry point
+*
+*Description: print alphabet 10 times
+*
+*Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (i = '0' ; i <= '9'; i++)
+	{
+		for (a = 'a' ; a <= 'z' ; a++)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
