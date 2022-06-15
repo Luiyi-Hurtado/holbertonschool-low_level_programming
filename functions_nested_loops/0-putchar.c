403373d348ffe8079dd3fb077fa8eb5d56a7c366
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-15 16:54:47 -0500

    created 0-putchar.c v1

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 *main - Entry point
 *
-*Description: print _putcar.
+*Description: print _putchar can
+*only use _putchar
 *
 *Return: 0
 */
 
 int main(void)
 {
-	printf("_putchar\n");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
 	return (0);
 }
