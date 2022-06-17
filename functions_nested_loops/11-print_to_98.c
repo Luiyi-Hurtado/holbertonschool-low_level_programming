ef26c65857cea1edf06fb6eb7064a2648038fb7d
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-17 14:31:36 -0500

    created 11-print_to_98.c v3

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *print_to_98 - Entry point
 *
@@ -15,12 +16,11 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				_putchar(n);
+				printf("%d, ", n);
 			}
 			else
 			{
-				_putchar(n);
-				_putchar('\n');
+				printf("%d\n", n);
 			}
 		}
 	}	
@@ -30,12 +30,11 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				_putchar(n);
+				printf("%d, ", n);
 			}
 			else
 			{
-				_putchar(n);
-				_putchar('\n');
+				printf("%d\n", n);
 			}
 		}
 	}
