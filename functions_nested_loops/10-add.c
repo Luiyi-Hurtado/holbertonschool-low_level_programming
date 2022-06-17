09b5f145756774add652c0351b523096ea4dd4ac
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-17 00:31:32 -0500

    created 10-add.c v1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*add - Entry point
+*
+*Description: addition n1 and n2 but
+*the result print with return.
+*
+*@n1: is int
+*@n2: is int
+*
+*Return: 0
+*/
+
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+
+	return (result);
+}
