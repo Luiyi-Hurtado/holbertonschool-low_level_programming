8473c41b50721eaf9fb09e9bfe9b7db36ea5f71e
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-17 00:07:39 -0500

    created 9-times_table.c v1

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
+void times_table(void)
+{
+	int i;
+	int o;
+	int r;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (o = 0 ; o <= 9 ; o++)
+		{
+			r = i * o;
+
+			if (r > 9)
+			{
+				_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
+			}
+			else 
+			{
+				if(o != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(r + '0');
+			}
+			if(o != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
