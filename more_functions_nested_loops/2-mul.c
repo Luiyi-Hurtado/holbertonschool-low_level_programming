7ce1240650d9f0cd43cbd40140d45f2b2e5e45d5
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-20 10:46:49 -0500

    created 2-mul.c v1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*mul - Entry point.c
+*@a: is to type int
+*@b: is to type int
+*Description: return the results of the
+*multipli of two integers
+*Return: 0
+*/
+
+int mul(int a, int b)
+{
+	int R;
+
+	R = a * b;
+	return (R);
+}
