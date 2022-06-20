99ccdbdfae145c026d6561400abd8b158798f12b
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-20 12:03:11 -0500

    created 5-more_numbers.c v1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*more_numbers - Entry point
+*Description: pritn numbers form 0 t0 14
+*and 1o times.
+*Return: 0
+*/
+
+void more_numbers(void)
+{
+	int i, o;
+
+	for ( o = '0' ; o <= '9' ; o++)
+	{
+		for (i = 0 ; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+		_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
