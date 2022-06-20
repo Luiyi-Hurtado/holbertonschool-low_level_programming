1cc361ca0e63bfb8bdf60acb897c387ceae8c307
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-20 16:07:19 -0500

    created 9-fizz_buzz.c v2

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	int i = 1;
 
-	for ( i = 1 ; i <= 100; i++)
+	for (i = 1 ; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0) 
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
