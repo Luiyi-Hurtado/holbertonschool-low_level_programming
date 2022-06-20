3f1fc9f973ab19b117b6f8d595603fe1d5238ac1
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-20 10:15:52 -0500

    created 0-isupper.c v1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_isupper - Entry pint
+*@c: is a type int
+*Description: the script returns 1 if the letters
+*is to uppercase and returns 0 if is otherwise
+*Returns: 0
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+return (0);
+}
