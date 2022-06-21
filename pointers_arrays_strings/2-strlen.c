22a07e97a51e9171ef46107a4557be8580ed44f4
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-21 17:38:24 -0500

    created 2-strlen.c v1

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen - Entry point
+ *@s: is to char type
+ *Description: returns the length of
+ *string
+ *Return: l length of a string
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0 ; s[l] ; l++)
+	{
+
+	}
+		return (l);
+}
