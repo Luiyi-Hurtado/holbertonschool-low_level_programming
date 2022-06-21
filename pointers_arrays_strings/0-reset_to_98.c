528b5e33d5e33b8282904f57c12e58c0dbc09f3f
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-21 15:55:05 -0500

    created 0-reset_to_98.c v1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - Entry point
+ *@n: is to type int
+ *Description: change the value
+ *to int parameter and update the value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
