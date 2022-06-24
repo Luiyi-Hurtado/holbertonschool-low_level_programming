3c2a945d272315f0d1482171709b6ca069a90c6d
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-23 22:43:28 -0500

    created 9-strcpy.c v1

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0 ; src[i] ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
