441d114ae8733281fdc53d86f5f9cef97ec17fb8
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-28 16:06:11 -0500

    created 4-rev_array.c v1

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *reverse_array - Entry point
+ *@a: pointer is type int
+ *@n: is type int
+ *Desription: reverse the content of an array
+ *Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		n--;
+		j = a[n];
+		a[n] = a[i];
+		a[i] = j;
+	}
+}
