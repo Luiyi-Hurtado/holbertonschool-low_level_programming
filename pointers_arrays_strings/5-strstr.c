8acf32a47bbb1bbeeacef94da09df6b30646e8c1
Luiyi_FHB <luiyifernando@gmail.com>
2022-06-29 18:37:56 -0500

    created 5-strstr.c v1

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strstr - Entry point
+ *@haystack: pointer is a char
+ *@needle: dpointer is a char
+ *Description: locates a substring
+ *Return: substring locates
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int k = 0;
+
+	for (i = 0 ; haystack[i] ; i++)
+	{
+		for (k = 0 ; needle[k] ; k++)
+		{
+			if (needle[k] == haystack[i + k])
+			{
+				break;
+			}
+		}
+		if (needle[k] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return ('\0');
+}
