68e167f2a40dea6724fcd44e55426dd161d3f563
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-01 17:06:53 -0500

    created 5-sqrt_recursion.c v1

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *Square_Root - Entry point
+ *@i: is type int
+ *@n: is type int
+ *Description: operation square root
+ *Return: square root
+ */
+int Square_Root(int n, int i)
+{
+	if (i <= n)
+	{
+		if (i * i == n)
+			return (i);
+		else
+			return (Square_Root(n, i + 1));
+	}
+	else
+		return (-1);
+}
+/**
+ *_sqrt_recursion - Entry point
+ *@n: is type int
+ *Description: returns the natural square root of a number
+ *Return: returns square root of a number
+ */
+int _sqrt_recursion(int n)
+{                                                                                                                                	if (n < 0)
+		return (-1);
+
+	return (Square_Root(n, 1));
+ }
