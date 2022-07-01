8641646da22c89d35bd3f100ffbbbd322a0c1153
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-01 14:51:36 -0500

    created 3-factorial.c v1

---
+++ recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *factorial - Entry point
+ *@n: is type int
+ *Description: returns the factorial 
+ *of a given number.
+ *Return: returns the factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if ( n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
