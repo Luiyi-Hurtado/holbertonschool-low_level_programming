691bd364c8c6c6c5a25e0b195372bf37612e08ca
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-11 08:51:50 -0500

    created script 0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *malloc_checked - use malloc
+ *@b: malloc
+ *
+ * Return: allocate b
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mallo;
+
+	mallo = malloc(b);
+	if (mallo == NULL)
+		exit (98);
+
+	return (mallo);
+}
