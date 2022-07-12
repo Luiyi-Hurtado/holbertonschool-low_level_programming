f4342493f7bddc20f22b597821524e640b3c6e3c
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-12 17:19:49 -0500

    change arithmetic funtction 3-function_like_macro.h

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO
-#define _3_FUNCTION_LIKE_MACRO
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x > 0) ? x : (x * -1)
+#define ABS(x) ((x) < 0 ? (-(x)) : ((x))
 
 #endif /* _3_FUNCTION_LIKE_MACRO */
