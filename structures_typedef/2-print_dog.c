8856d4e5d9dcc4e160c41a9328180dfaf3e404e7
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-13 14:31:05 -0500

    add new line in name dog and name owner 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,13 +14,13 @@ void print_dog(struct dog *d)
 		if ((*d).name != NULL)
 			printf("Name: %s\n", (*d).name);
 		else
-			printf("Name: (nil)\0");
+			printf("Name: (nil)\n");
 
 		printf("Age: %.6f\n", (*d).age);
 
 		if ((*d).owner != NULL)
 			printf("Name: %s\n", (*d).owner);
 		else
-			printf("Name: (nil)\0");
+			printf("Name: (nil)\n");
 	}
 }
