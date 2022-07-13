dc7f842add664e6a724c8280be870d855b97a9a4
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-13 12:12:43 -0500

    add space in if 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -5,13 +5,13 @@
  *@d: pointer of struct dog
  *@name: pointer name dog
  *@age: age of the dog
- *owner: pointer owner dog
+ *@owner: pointer owner dog
  *
  *Return: Always 0
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d)
+	if (d)
 	{
 		(*d).name = name;
 		(*d).age = age;
