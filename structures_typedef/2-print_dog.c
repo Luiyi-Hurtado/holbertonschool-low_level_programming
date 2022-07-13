9906f0dcda146122638293c182c113c256b78d91
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-13 14:21:31 -0500

    created script 2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *print_dog - print info about the dog
+ *@d: pointer struct the dogs
+ *
+ *return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if ((*d).name != NULL)
+			printf("%s\n", (*d).name);
+		else
+			printf("Name: (nil)");
+
+		printf("%.2f\n", (*d).age);
+
+		if ((*d).owner != NULL)
+			printf("%s\n", (*d).owner);
+		else
+			printf("Name : (nil)");
+	}
+}
