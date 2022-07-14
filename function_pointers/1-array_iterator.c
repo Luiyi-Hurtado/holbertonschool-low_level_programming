77d6ef8e8f9afccbeb0b5baa8ee0656e812f3944
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-14 11:32:08 -0500

    change script 1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,19 @@
 #include "function_pointers.h"
 
+/**
+ *array_iterartor - iterator array
+ *@array: pointer array
+ *@size: size of array
+ *@action: pointer function
+ *
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 
 {
 	size_t i = 0;
 
-	if (action == NULL)
-		return (NULL);
+	if (action != NULL)
+
 	for (i = 0 ; i <= size ; i++)
 		action(array[i]);
 }
