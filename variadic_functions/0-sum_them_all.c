00a596bed3cbbfa52cbd920ed75457ca5401470b
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-15 15:02:51 -0500

    created script 0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all - sum of all its parameters
+ *@n: fixed argument
+ *
+ *Return: sum of arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+	int sum = 0;
+
+	va_start(ap, n);
+
+	if (n != 0)
+	{
+		for (i = 0 ; i < n ; i++)
+			sum += va_arg(ap, const unsigned int);
+	}
+	else
+		return (0);
+
+	return (sum);
+}
