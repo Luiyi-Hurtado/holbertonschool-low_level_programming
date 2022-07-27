249dafa194aa07772c15247da8d1cf05341f500d
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-27 13:15:01 -0500

    add pointer to list 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,8 +12,8 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h == NULL)
-			printf("[0] (nil)");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		i++;
