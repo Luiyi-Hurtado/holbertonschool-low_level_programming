efed9ef9d7484616df3cb9e29900229d38b8942c
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-27 16:53:45 -0500

    create script 0-print_listint.c

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *print_listint - print elements of list
+ *@h: header pointer of the list
+ *
+ *Return: elementes of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	if (h != NULL)
+	{
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
+	}
+
+	return (count);
+}
