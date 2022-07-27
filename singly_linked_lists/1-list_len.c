1acc4d19b2bbad28c2cf2a87ea5b4cfcc298b170
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-27 13:44:06 -0500

    created script 1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ *list_len - number elements in the list
+ *@h: header pointer
+ *
+ *Return: number elements
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+
+	for (count = 0 ; h != NULL ; count++)
+		h = h->next;
+	return (count);
+}
