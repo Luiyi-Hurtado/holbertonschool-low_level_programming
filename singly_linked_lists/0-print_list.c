98078061de6223ac2c03568d4a128d68a61cf5e5
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-27 13:54:19 -0500

    visual rest

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -16,9 +16,11 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		i++;
 		h = h->next;
 	}
+
 	return (i);
 }
 
--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -12,5 +12,6 @@ size_t list_len(const list_t *h)
 
 	for (count = 0 ; h != NULL ; count++)
 		h = h->next;
+
 	return (count);
 }
