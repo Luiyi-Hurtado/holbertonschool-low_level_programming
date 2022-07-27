f78b032ea2c1f13ba520619548d67c6320239201
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-27 12:58:45 -0500

    created script 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *print_list - print all the elements
+ *@h: header pointer
+ *
+ *Return: always 0
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		if (h == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
