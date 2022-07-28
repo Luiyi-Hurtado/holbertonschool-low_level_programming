c14e6161794c621b03e09443aa2fab96651c8600
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-28 18:06:37 -0500

    created script 1-listint_len.c

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *listint_len -  returns the number of elements of the list.
+ *@h: header pointer of the linked list
+ *
+ *Return: numbers of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	
+	return (count);
+}
