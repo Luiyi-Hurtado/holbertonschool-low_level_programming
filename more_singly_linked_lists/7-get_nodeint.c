a1ffa8a13e3b6bc58268f02cbf1e182921f4eb72
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-29 17:10:28 -0500

    created script 7-get_nodeint.c

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - returns the nth node of a list
+ *@head: header pointer of the linked list
+ *index: index of the node
+ *
+ * Return: NULL if the head is NULL or head
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	for (count = 0; count < index; count++)
+	{
+		if (!head)
+			return (NULL);
+		head = head->next;
+	}
+
+	return (head);
+}
