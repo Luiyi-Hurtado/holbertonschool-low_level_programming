f1ae60072e79314c3d4ad424adb04ccd3caecefd
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-29 12:27:03 -0500

    created script 5-free_listint2.c

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - free list
+ *@head: header pointer of the linked list
+ *
+ *Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	while (*head)
+	{
+		new = (*head)->next;
+		free(head);
+		*head = new;
+	}
+
+	*head = NULL;
+}
