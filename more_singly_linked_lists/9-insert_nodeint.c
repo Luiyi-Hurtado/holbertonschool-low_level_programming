57c822b7de93245224a80343fd7c55e64e5c5199
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-29 19:12:51 -0500

    add !copy_h->next in the line 33

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (count = 0; count < idx; count++)
 	{
-		if (!copy_h)
+		if (!copy_h || !copy_h->next)
 			return (NULL);
 		copy_h = copy_h->next;
 	}
