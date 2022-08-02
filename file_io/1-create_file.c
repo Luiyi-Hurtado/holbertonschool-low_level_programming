350d99d7ad7720f0c8bef2fa218f3dedde4f0f6d
Luiyi_FHB <luiyifernando@gmail.com>
2022-08-02 15:23:34 -0500

    betty style

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *create_file - create a file
+ *@filename: name of the filename
+ *@text_content: conten of the file
+ *
+ * Return: create file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int opn, wrt, slen = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[slen])
+			slen++;
+	}
+
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrt = write(opn, text_content, slen);
+
+	if (opn == -1 || wrt == -1)
+		return (-1);
+
+	close(opn);
+
+	return (1);
+}
