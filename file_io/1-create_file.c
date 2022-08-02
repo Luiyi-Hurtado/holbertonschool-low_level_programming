7e15039b650a9a60e626f1dbeef9c2becfa471f8
Luiyi Hurtado <75503930+Luiyi-F@users.noreply.github.com>
2022-08-02 15:33:23 -0500

    update
    changes

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,10 +2,10 @@
 
 /**
  *create_file - create a file
- *@filename: name of the filename
+ *@filename: name of the file
  *@text_content: conten of the file
  *
- * Return: create file
+ * Return: 1 or -1
  */
 int create_file(const char *filename, char *text_content)
 {
