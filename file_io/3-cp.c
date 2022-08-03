c0960a822da47076abdd7ce798ca1efc76b22c07
Luiyi_FHB <luiyifernando@gmail.com>
2022-08-03 12:50:17 -0500

    upgrade 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,26 +12,29 @@ int main(int argc, char **argv)
 	int f_from, f_to, _rd, _wr;
 	char buffer[1024];
 
-	f_from = open(argv[1], O_RDONLY);
-	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	_rd = read(f_from, buffer, 1024);
-	_wr = write(f_to, buffer, _rd);
 
 	if (argc != 3)
 	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
+
+	f_from = open(argv[1], O_RDONLY);
 	if (f_from == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
+
+	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (f_to == -1)
 	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 
 	while (_rd != 0)
 	{
+		_rd = read(f_from, buffer, 1024);
 		if (_rd == -1)
-		{dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+		{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		}
+
+		_wr = write(f_to, buffer, _rd);
 		if (_wr == -1)
 		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
@@ -43,6 +46,5 @@ int main(int argc, char **argv)
 	if (close(f_to) == -1)
 	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from), exit(100);
 	}
-
 	return (0);
 }
