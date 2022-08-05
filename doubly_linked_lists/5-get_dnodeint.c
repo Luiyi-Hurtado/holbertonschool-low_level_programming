ad3d4aad07da597cfae26fc0203fb7517690b58d
Luiyi_FHB <luiyifernando@gmail.com>
2022-08-04 20:45:54 -0500

    returns the nth node

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *get_dnodeint_at_index - returns the nth node of a linked list
+ *@head: header prointer of the linked
+ *@index: index of the node
+ *
+ *Return: NULL if the head is NULL or the head
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	for (count = 0; count < index; count++)
+	{
+		if (!head)
+			return (NULL);
+		head = head->next;
+	}
+	
+	return (head);
+}
