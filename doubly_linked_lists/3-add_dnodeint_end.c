300eaa0eff6f86f676432e5bb3d40df9e905cf88
Luiyi_FHB <luiyifernando@gmail.com>
2022-08-04 20:04:06 -0500

    update

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_n, *last_n;
 
-	new_n = malloc(sizeof(dlistint_s));
+	new_n = malloc(sizeof(dlistint_t));
 	if (!new_n)
 		return (NULL);
 
