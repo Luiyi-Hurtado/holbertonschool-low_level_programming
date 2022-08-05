97d1da6e0396db3c0c9bf2f925dc03594333a8f0
Luiyi_FHB <luiyifernando@gmail.com>
2022-08-04 20:15:48 -0500

    upgrade the script

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -25,12 +25,12 @@ int main(int argc, char **argv)
 
 	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (f_to == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]); 
+		exit(99);
 	}
 
-	while (_rd != 0)
+	while ((_rd = read(f_from, buffer, 1024))!= 0)
 	{
-		_rd = read(f_from, buffer, 1024);
 		if (_rd == -1)
 		{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
