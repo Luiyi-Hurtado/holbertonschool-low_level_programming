e3018261107fde0a846779f7677c8a653cbdf265
Luiyi_FHB <luiyifernando@gmail.com>
2022-08-04 20:27:59 -0500

    betty style

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
+ *free_dlistint - that frees a list.
+ *@head: header pointer of the linked list
  *
- *
- *
- *
+ *Return: nothing
  */
 void free_dlistint(dlistint_t *head)
 {
