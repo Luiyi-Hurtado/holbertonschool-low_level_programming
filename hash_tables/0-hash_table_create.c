1c42c923260659c71668c72d59a1ecaff2b5addf
luiyi_V <luiyifernando5@gmail.com>
2022-08-29 14:46:14 -0500

    betty style

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - Create memory space of hash tables
+ *@size: Hash table index
+ *
+ *Return: New hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	size_t indx = 0;
+
+	if (size > 0)
+	{
+		new_table = malloc(sizeof(hash_table_t));
+		if (!new_table)
+			return (NULL);
+
+		new_table->size = size;
+		new_table->array = malloc(sizeof(hash_table_t *) * size);
+		if (!new_table->array)
+			return (NULL);
+
+		for (indx = 0; indx < size; indx++)
+			new_table->array[indx] = NULL;
+
+		return (new_table);
+	}
+	return (NULL);
+}
