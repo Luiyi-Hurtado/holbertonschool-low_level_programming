06a761c76700bdd394303c784f4f6202b60c0b87
luiyi_V <luiyifernando5@gmail.com>
2022-08-30 09:26:28 -0500

    betty style

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,27 +1,28 @@
 #include "hash_tables.h"
 
 /**
+ *hash_table_get - give key and gey value
+ *@ht: hash table
+ *@key: key of the value
  *
- *
- *
- *
+ *Return: Value
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-        unsigned long int indx = 0;
-        hash_node_t *new_element;
+	unsigned long int indx = 0;
+	hash_node_t *new_element;
 
-        if (!ht || !key)
-                return (NULL);
+	if (!ht || !key)
+		return (NULL);
 
-        indx = key_index((const unsigned char *) key, ht->size);
-        new_element = (ht->array)[indx];
+	indx = key_index((const unsigned char *) key, ht->size);
+	new_element = (ht->array)[indx];
 
-        while (new_element != NULL && strcmp(new_element->key, key) != 0)
-                new_element = new_element->next;
+	while (new_element != NULL && strcmp(new_element->key, key) != 0)
+		new_element = new_element->next;
 
-        if (!new_element)
-                return (NULL);
-        else
-                return (new_element->value);
+	if (!new_element)
+		return (NULL);
+	else
+		return (new_element->value);
 }
