0982e373cf5309253b0fc76de6afedfb6b0a32b2
luiyi_V <luiyifernando5@gmail.com>
2022-09-08 09:41:49 -0500

    fixed name header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifdef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+/* library */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* prototype */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_H_ */
