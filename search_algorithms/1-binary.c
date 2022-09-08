2e018363bbda9b485e97175deabbd9a05960f9d2
luiyi_V <luiyifernando5@gmail.com>
2022-09-08 10:26:36 -0500

    betty syle

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -12,34 +12,34 @@
  */
 int middle(int *array, int low, int high, size_t size, int value)
 {
-    int middle = 0, indx = 0;
+	int middle = 0, indx = 0;
 
-    low = 0;
-    high = size - 1;
+	low = 0;
+	high = size - 1;
 
-    if (array != NULL)
-    {
-        while (low <= high)
-        {
-            middle = (low + high) / 2;
-            printf("Searching in array: ");
-            for (indx = low; indx <= high; indx++)
-            {
-                if (indx == high)
-                    printf("%d", array[indx]);
-                if (indx != high)
-                    printf("%d, ", array[indx]);
-            }
-        printf("\n");
-        if (value < array[middle])
-            high = middle - 1;
-        if (value > array[middle])
-            low = middle + 1;
-        if (value == array[middle])
-            return (middle);
-        }
-    }
-    return (-1);
+	if (array != NULL)
+	{
+		while (low <= high)
+		{
+			middle = (low + high) / 2;
+			printf("Searching in array: ");
+			for (indx = low; indx <= high; indx++)
+			{
+				if (indx == high)
+					printf("%d", array[indx]);
+				if (indx != high)
+					printf("%d, ", array[indx]);
+			}
+			printf("\n");
+			if (value < array[middle])
+				high = middle - 1;
+			if (value > array[middle])
+				low = middle + 1;
+			if (value == array[middle])
+				return (middle);
+		}
+	}
+	return (-1);
 }
 
 /**
@@ -52,12 +52,12 @@ int middle(int *array, int low, int high, size_t size, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    int min = 0, max = 0;
+	int min = 0, max = 0;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    min = 0;
-    max = size - 1;
-    return (middle(array, min, max, size, value));
+	min = 0;
+	max = size - 1;
+	return (middle(array, min, max, size, value));
 }
