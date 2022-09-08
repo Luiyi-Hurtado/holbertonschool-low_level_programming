ccd830c4adf5f047723cf3f7b3b4de8ba6c462fd
luiyi_V <luiyifernando5@gmail.com>
2022-09-08 10:02:36 -0500

    update header file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -9,5 +9,6 @@
 /* prototype */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int middle(int *array, int low, int high, size_t size, int value);
 
 #endif /* _SEARCH_ALGOS_H_ */
