01606d11f8539feaf10319378a0a4a9a797bf03f
luiyi_V <luiyifernando5@gmail.com>
2022-09-08 09:57:11 -0500

    betty style

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,22 +6,22 @@
  *@array: the array
  *@size: size of the array
  *@value: value of the array
- *Return: -1 or index 
+ *Return: -1 or index
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t indx = 0;
+	size_t indx = 0;
 
-    if (array)
-    {
-        for (indx = 0; indx < size; indx++)
-        {
-            printf("Value checked array[%ld] = [%d]\n", indx, array[indx]);
-            if (array[indx] == value)
-            {
-                return (indx);
-            }
-        }
-    }
-    return (-1);
+	if (array)
+	{
+		for (indx = 0; indx < size; indx++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", indx, array[indx]);
+			if (array[indx] == value)
+			{
+				return (indx);
+			}
+		}
+	}
+	return (-1);
 }
