4a9aaee568b07e2af6ac53f8dbdd7890ee80f780
luiyi_V <luiyifernando5@gmail.com>
2022-09-08 09:54:07 -0500

    add comments

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ *linear_search - search algorithm
+ *
+ *@array: the array
+ *@size: size of the array
+ *@value: value of the array
+ *Return: -1 or index 
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t indx = 0;
+
+    if (array)
+    {
+        for (indx = 0; indx < size; indx++)
+        {
+            printf("Value checked array[%ld] = [%d]\n", indx, array[indx]);
+            if (array[indx] == value)
+            {
+                return (indx);
+            }
+        }
+    }
+    return (-1);
+}
